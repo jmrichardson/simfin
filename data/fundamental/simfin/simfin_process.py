cb1b6100acce4f4192e60a38daeaaee76a7dfad8
John Richardson <jmrichardson@gmail.com>
2019-03-17 09:46:01 -0400

    Fixed process

--- data/fundamental/simfin/simfin_process.py
+++ data/fundamental/simfin/simfin_process.py
@@ -3,6 +3,7 @@ import talib
 import pandas as pd
 import numpy as np
 from loguru import logger
+import random
 
 # Set current directory
 try:
@@ -42,7 +43,12 @@ logger.info("Dataset rows: " + str(rows))
 
 # Temporarily make simfin dataset smaller for testing
 # simfin = simfin.query('Ticker == "A" | Ticker == "AAMC"')
-simfin = simfin.query('Ticker == "ACM"')
+simfin = simfin.query('Ticker == "FLWS"')
+
+
+indicators = ['Revenues', 'COGS']
+df (TTM):
+df['Revenues'].rolling(12, min_periods=1).sum()
 
 
 # Process data by ticker
@@ -57,31 +63,33 @@ def byTicker(df):
     # Fill Share Price NAs with last known value
     df['Share Price'] = df['Share Price'].ffill()
 
-    # Add average monthly share price column
-    df.insert(3, column='Share Price Monthly Average', value=df.groupby(pd.Grouper(key='Date', freq='M'))['Share Price'].transform('mean'))
+    # Average share prices for last 30 days
+    df.insert(3, column='Share Price Monthly Average', value=df['Share Price'].rolling(30, min_periods=1).mean())
+    try:
+        df.insert(4, column='MOM1MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 30))
+        df.insert(5, column='MOM2MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 60))
+        df.insert(6, column='MOM3MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 90))
+        df.insert(7, column='MOM6MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 180))
+        df.insert(8, column='MOM9MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 270))
+        df.insert(9, column='MOM12MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 360))
+    except:
+        pass
 
-    # Last not null value in previous x days
-    df.iloc[:, 3:] = df.iloc[:, 3:].rolling(92, min_periods=1).apply(lambda x: x[x.last_valid_index()], raw=False)
+    # Get Last known value (these fields are reported at different times in the quarter by simfin)
+    # This will get the last value within 90 days to be used by rows with published quarter data
+    # df.iloc[:, 3:] = df.iloc[:, 3:].rolling(92, min_periods=1).apply(lambda x: x[x.last_valid_index()], raw=False)
+    df['Common Shares Outstanding'] = df['Common Shares Outstanding'].rolling(92, min_periods=1).apply(lambda x: x[x.last_valid_index()], raw=False)
+    df['Avg. Basic Shares Outstanding'] = df['Avg. Basic Shares Outstanding'].rolling(92, min_periods=1).apply(lambda x: x[x.last_valid_index()], raw=False)
+    df['Avg. Diluted Shares Outstanding'] = df['Avg. Diluted Shares Outstanding'].rolling(92, min_periods=1).apply(lambda x: x[x.last_valid_index()], raw=False)
 
-    # Reduce to monthly by getting last info/row per month
-    df = df.groupby(pd.Grouper(key='Date', freq='M')).tail(1)
+    # Remove rows where column val is Null (Only rows with quarterly report published)
+    df = df[df['Revenues'].notnull()]
 
     # Remove rows with too many null values
     df = df.dropna(axis=0, thresh=15, subset=df.columns.to_list()[3:])
 
-    # Remove rows where Share Price Monthly Average is Null
-    df = df[df['Share Price Monthly Average'].notnull()]
-
-    # Add technical indicators
-    if len(df) >= 1:
-        df.insert(4, column='MOM1MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 1))
-        df.insert(5, column='MOM2MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 2))
-        df.insert(6, column='MOM3MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 3))
-        df.insert(7, column='MOM6MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 6))
-        df.insert(8, column='MOM9MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 9))
-        df.insert(9, column='MOM12MA', value=talib.MOM(np.array(df['Share Price Monthly Average']), 12))
-    else:
-        logger.info("WARNING - No rows: " + ticker)
+    # Trailing values
+    df['Revenues TTM'] = df['Revenues'].rolling(12, min_periods=1).sum()
 
     return df
 
@@ -89,7 +97,7 @@ logger.info("Grouping SimFin data by ticker...")
 data = simfin.groupby('Ticker').apply(byTicker)
 
 # Save dataset output
-data.to_csv('data.csv', encoding='utf-8', index=False)
+data.to_csv('data' + str(random.randint(1, 100000)) + '.csv', encoding='utf-8', index=False)
 
 
 
